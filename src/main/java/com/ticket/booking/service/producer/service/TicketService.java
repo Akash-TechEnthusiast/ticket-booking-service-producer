1297ce0fe584c36c130d6c805cc2458c1b28cbac
akash922.g@gmail.com <akash922.g@gmail.com>
2025-08-24 15:26:03 +0530

    ensure message is deliver

--- src/main/java/com/ticket/booking/service/producer/service/TicketService.java
+++ src/main/java/com/ticket/booking/service/producer/service/TicketService.java
@@ -6,8 +6,11 @@ import com.ticket.booking.service.producer.entity.TicketBooking;
 import com.ticket.booking.service.producer.repo.TicketBookingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @Service
@@ -30,8 +33,27 @@ public class TicketService {
                 saved.getId(), saved.getPassengerName(), saved.getTrainId(), "BOOKED");
 
 
-        kafka.send("ticket.booked", saved.getId().toString(), event);
-        saved.setStatus("SENT");
+        //kafka.send("ticket.booked", saved.getId().toString(), event);
+
+
+        CompletableFuture<SendResult<String, TicketBookedEvent>> future =
+                kafka.send("ticket.booked", saved.getId().toString(), event);
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("✅ Sent message to topic: " +
+                        result.getRecordMetadata().topic() +
+                        " partition: " + result.getRecordMetadata().partition() +
+                        " offset: " + result.getRecordMetadata().offset());
+                saved.setStatus("SENT");
+            } else {
+                System.err.println("❌ Failed to send message: " + ex.getMessage());
+                saved.setStatus("Failed to SENT");
+            }
+        });
+
         return repo.save(saved);
+
+
     }
 }
